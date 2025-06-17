56d79b0b63776ce249040b8b60203d747420b1f5
whals <whalsdud0131@gmail.cm>
2025-06-17 10:26:27 +0900

    lambda

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+@FunctionalInterface
+interface MathOperation {
+    int operate(int a, int b);
+}
+
 public class Main {
     public static void main(String[] args) {
+        MathOperation add = (a, b) -> a + b;
+        MathOperation multiply = (a, b) -> a * b;
+
+        System.out.println(add.operate(2,4));
+        System.out.println(multiply.operate(3,5));
 
     }
 }
